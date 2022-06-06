f628970895707a899db352b6b00a6ddf08fa73d6
Ali Aghahaji <reven2on@hotmail.com>
2022-06-06 08:48:41 +0430

    localization file created

---
+++ lang/en/messages.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'article_created' => 'The article has been created successfully',
+    'article_loaded' => 'The article has been loaded',
+    'article_not_found' => 'No article available for this ID',
+    'articles_loaded' => 'The article has been loaded',
+    'article_rated' => 'The article rated successfully',
+    'article_rated_before' => 'The article has been rated before by current IP address',
+    'rate_limiter_too_many_attemps' => 'Too many attempts, try again later',
+    'reach_daily_rating_limit'  => 'You have reached your daily article rating limit',
+
+    'article_validation_wrong_popularity_sort_param' => 'Sort by popularity only accepts `view` or `rating`',
+    'article_validation_wrong_category' => 'Some of category/categories are wrong',
+    'article_validation_wrong_date_format'  => 'Date range does not match the format Y-m-d.',
+];
