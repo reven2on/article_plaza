e1f0995f234b1048af6c869dc0f7e61587558e63
Ali Aghahaji <reven2on@hotmail.com>
2022-06-06 08:48:15 +0430

    migrations modified

--- database/migrations/2022_06_04_004318_create_articles_table.php
+++ database/migrations/2022_06_04_004318_create_articles_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('body');
+            $table->double('calculated_rate', 8, 2)->default(0);
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 };
